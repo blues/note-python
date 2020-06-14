6666804d1e011e89b38c963c417cbe6cb5a979e9
Brandon Satrom <bsatrom+gh@gmail.com>
2020-06-14 11:40:23 -0500

    added mocks for Serial operations

--- test/test_notecard.py
+++ test/test_notecard.py
@@ -1,11 +1,19 @@
 import os
 import sys
+import serial
+
+from unittest.mock import Mock, MagicMock, patch
 
 sys.path.insert(0, os.path.abspath(
                 os.path.join(os.path.dirname(__file__), '..')))
 
 import notecard  # noqa: E402
 
-
 def test_open_serial():
-    assert True
+    serial = Mock()
+    port = serial.Serial("/dev/tty.foo", 9600)
+    port.read.side_effect = [b'\r', b'\n', None]
+
+    card = notecard.OpenSerial(port)
+    
+    assert card.uart is not None
