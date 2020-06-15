59d550023c96ad77ea437fbd54714127552a3045
Brandon Satrom <bsatrom+gh@gmail.com>
2020-06-15 14:45:18 -0500

    add coverage, MAKEFILE for tests, and clean-up test style

--- test/test_notecard.py
+++ test/test_notecard.py
@@ -10,20 +10,22 @@ sys.path.insert(0, os.path.abspath(
 
 import notecard  # noqa: E402
 
+
 def test_open_serial():
-    serial = Mock()
+    serial = Mock()  # noqa: F811
     port = serial.Serial("/dev/tty.foo", 9600)
     port.read.side_effect = [b'\r', b'\n', None]
 
     card = notecard.OpenSerial(port)
-    
+
     assert card.uart is not None
 
+
 def test_open_i2c():
-    periphery = Mock()
+    periphery = Mock()  # noqa: F811
     port = periphery.I2C("dev/i2c-foo")
     port.try_lock.return_value = True
-    
+
     card = notecard.OpenI2C(port, 0x17, 255)
-    
+
     assert card.i2c is not None
