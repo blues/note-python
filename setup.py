8d388bc44a9f15ff4c6f2a76c5e54a3782a92525
Brandon Satrom <bsatrom+gh@gmail.com>
2020-07-24 14:07:35 -0500

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="note-python",
-    version="1.1.0",
+    version="1.1.1",
     author="Blues Inc.",
     author_email="hello@blues.io",
     description="Cross-platform Python Library for the Blues Wireless Notecard,",
