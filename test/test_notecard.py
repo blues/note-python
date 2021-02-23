513f320228dd6ebc33766daa73740c92221f3051
Greg Wolff <gwolff@blues.com>
2021-02-23 15:56:30 -0500

    updates half tests to use 'readline' mock instead of 'read'

--- test/test_notecard.py
+++ test/test_notecard.py
@@ -17,7 +17,7 @@ def get_serial_and_port():
     serial = Mock()  # noqa: F811
     port = serial.Serial("/dev/tty.foo", 9600)
     port.read.side_effect = [b'\r', b'\n', None]
-    port.read.side_effect = [b'\r', b'\n', None]
+    port.readline.return_value = "\r\n"
 
     nCard = notecard.OpenSerial(port)
 
@@ -49,10 +49,7 @@ def test_open_i2c():
 def test_transaction():
     nCard, port = get_serial_and_port()
 
-    port.read.side_effect = [char.encode('utf-8')
-                             for char in "{\"connected\":true}\r\n"]
-
-    port.readline.side_effect = "{\"connected\":true}\r\n"
+    port.readline.return_value = "{\"connected\":true}\r\n"
     print(port.readline())
     response = nCard.Transaction({"req": "hub.status"})
 
@@ -78,9 +75,7 @@ def test_command_fail_if_req():
 def test_hub_set():
     nCard, port = get_serial_and_port()
 
-    port.read.side_effect = [char.encode('utf-8')
-                             for char in "{}\r\n"]
-
+    port.readline.return_value = "{}\r\n"
     response = hub.set(nCard, product="com.blues.tester",
                        sn="foo",
                        mode="continuous",
@@ -104,9 +99,7 @@ def test_hub_set_invalid_card():
 def test_hub_sync():
     nCard, port = get_serial_and_port()
 
-    port.read.side_effect = [char.encode('utf-8')
-                             for char in "{}\r\n"]
-
+    port.readline.return_value = "{}\r\n"
     response = hub.sync(nCard)
 
     assert response == {}
@@ -115,8 +108,7 @@ def test_hub_sync():
 def test_hub_sync_status():
     nCard, port = get_serial_and_port()
 
-    port.read.side_effect = [char.encode('utf-8')
-                             for char in "{\"status\":\"connected\"}\r\n"]
+    port.readline.return_value = "{\"status\":\"connected\"}\r\n"
 
     response = hub.syncStatus(nCard, True)
 
@@ -127,8 +119,7 @@ def test_hub_sync_status():
 def test_hub_status():
     nCard, port = get_serial_and_port()
 
-    port.read.side_effect = [char.encode('utf-8')
-                             for char in "{\"connected\":true}\r\n"]
+    port.readline.return_value = "{\"connected\":true}\r\n"
 
     response = hub.status(nCard)
 
@@ -139,8 +130,7 @@ def test_hub_status():
 def test_hub_log():
     nCard, port = get_serial_and_port()
 
-    port.read.side_effect = [char.encode('utf-8')
-                             for char in "{}\r\n"]
+    port.readline.return_value = "{}\r\n"    
 
     response = hub.log(nCard, "there's been an issue!", False)
 
@@ -150,8 +140,7 @@ def test_hub_log():
 def test_hub_get():
     nCard, port = get_serial_and_port()
 
-    port.read.side_effect = [char.encode('utf-8')
-                             for char in "{\"mode\":\"continuous\"}\r\n"]
+    port.readline.return_value = "{\"mode\":\"continuous\"}\r\n"
 
     response = hub.get(nCard)
 
@@ -162,9 +151,7 @@ def test_hub_get():
 def test_card_time():
     nCard, port = get_serial_and_port()
 
-    port.read.side_effect = [char.encode('utf-8')
-                             for char in
-                             "{\"time\":1592490375}\r\n"]
+    port.readline.return_value ="{\"time\":1592490375}\r\n"
 
     response = card.time(nCard)
 
@@ -175,9 +162,7 @@ def test_card_time():
 def test_card_status():
     nCard, port = get_serial_and_port()
 
-    port.read.side_effect = [char.encode('utf-8')
-                             for char in
-                             "{\"usb\":true,\"status\":\"{normal}\"}\r\n"]
+    port.readline.return_value = "{\"usb\":true,\"status\":\"{normal}\"}\r\n"
 
     response = card.status(nCard)
 
@@ -188,9 +173,7 @@ def test_card_status():
 def test_card_temp():
     nCard, port = get_serial_and_port()
 
-    port.read.side_effect = [char.encode('utf-8')
-                             for char in
-                             "{\"value\":33.625,\"calibration\":-3.0}\r\n"]
+    port.readline.return_value = "{\"value\":33.625,\"calibration\":-3.0}\r\n"
 
     response = card.temp(nCard, minutes=20)
 
@@ -201,9 +184,7 @@ def test_card_temp():
 def test_card_attn():
     nCard, port = get_serial_and_port()
 
-    port.read.side_effect = [char.encode('utf-8')
-                             for char in
-                             "{\"set\":true}\r\n"]
+    port.readline.return_value = "{\"set\":true}\r\n"
 
     response = card.attn(nCard, mode="arm, files",
                          files=["sensors.qo"],
@@ -222,9 +203,7 @@ def test_card_attn_with_invalid_card():
 def test_card_voltage():
     nCard, port = get_serial_and_port()
 
-    port.read.side_effect = [char.encode('utf-8')
-                             for char in
-                             "{\"hours\":707}\r\n"]
+    port.readline.return_value = "{\"hours\":707}\r\n"
 
     response = card.voltage(nCard, hours=24, offset=5, vmax=4, vmin=3)
 
@@ -235,9 +214,7 @@ def test_card_voltage():
 def test_card_wireless():
     nCard, port = get_serial_and_port()
 
-    port.read.side_effect = [char.encode('utf-8')
-                             for char in
-                             "{\"status\":\"{modem-off}\",\"count\":1}\r\n"]
+    port.readline.return_value = "{\"status\":\"{modem-off}\",\"count\":1}\r\n"
 
     response = card.wireless(nCard, mode="auto", apn="-")
 
@@ -248,9 +225,7 @@ def test_card_wireless():
 def test_card_version():
     nCard, port = get_serial_and_port()
 
-    port.read.side_effect = [char.encode('utf-8')
-                             for char in
-                             "{\"version\":\"notecard-1.2.3.9950\"}\r\n"]
+    port.readline.return_value = "{\"version\":\"notecard-1.2.3.9950\"}\r\n"
 
     response = card.version(nCard)
 
