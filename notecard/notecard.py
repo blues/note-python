d59b9df675bb5d60106f673e3a73057dfe7cb3a1
Rob Lauer <rdlauer@gmail.com>
2022-08-03 12:53:03 -0500

    more linting fixes

--- notecard/notecard.py
+++ notecard/notecard.py
@@ -67,8 +67,8 @@ if not use_rtc:
         from supervisor import ticks_ms
 
         _TICKS_PERIOD = 1 << 29
-        _TICKS_MAX = _TICKS_PERIOD-1
-        _TICKS_HALFPERIOD = _TICKS_PERIOD//2
+        _TICKS_MAX = _TICKS_PERIOD - 1
+        _TICKS_HALFPERIOD = _TICKS_PERIOD // 2
 
         def ticks_diff(ticks1, ticks2):
             """Compute the signed difference between two ticks values."""
