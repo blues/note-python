a7183b3c9ffce1b60e691d337f446383e5a09aef
Matthew McGowan <mat.mcgowan+git@gmail.com>
2023-07-31 10:51:55 -0700

    upgrade PyYML to work around a build problem on python 3.11.

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.11",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Intended Audience :: Developers",
@@ -28,3 +29,4 @@ setuptools.setup(
     install_requires=["filelock"],
     python_requires='>=3.6',
 )
+
